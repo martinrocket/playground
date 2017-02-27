33be1ee8a8abe0c91829768fa0bfe49297d895fb
martinrocket <martin.ruble@gmx.com>
2017-02-27 16:00:39 -0700

    learned json/marshal and marshalindent

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
 )
+
+type MyList struct {
+	Id     int            `json:"id"`
+	Status string         `json:"status"`
+	Table  string         `json:"table"`
+	Name   string         `json:"name"`
+	Items  map[int]string `json:"items"`
+}
 
 func main() {
 	fmt.Println("Hello Gophers!")
@@ -13,6 +22,7 @@ func main() {
 	fmt.Println(myTime.Format("Jan _2 15:04:05.000"))
 	fmt.Println(myTime.Format("01_02_15:04:05.000"))
 	fmt.Println(myTime.Format("01_02_2006"))
+	writeJson()
 	fmt.Print("Enter text: ")
 	//var text []byte
 	//fmt.Scanln(&text)
@@ -31,4 +41,26 @@ func main() {
 
 	fmt.Println(myText)
 
+}
+
+func writeJson() {
+
+	s := MyList{
+		Id:     1,
+		Status: "open",
+		Table:  "32",
+		Name:   "Jana Miller",
+		Items: map[int]string{
+			1: "Coke",
+			2: "Potatoes",
+			3: "Pie",
+		},
+	}
+	jCk, err := json.MarshalIndent(s, " ", "    ")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(jCk))
 }
