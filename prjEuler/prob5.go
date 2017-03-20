87f4caa32c8707c82a1435c78af73c780456522f
martinrocket <martin.ruble@gmx.com>
2017-03-19 21:51:19 -0600

    add prob 5 and 6

---
+++ prjEuler/prob5.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+/*
+
+Smallest multiple
+Problem 5
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+ */
+
+var x int
+var y int = 0
+
+func main() {
+	myBool := false
+	for {
+		x = getNext()
+		myBool = isItIt(x)
+		if myBool == true {
+			fmt.Println("Your number is ", x)
+			break
+		}
+	}
+
+
+}
+
+
+
+
+func getNext() int {
+	y += 20
+	return y
+
+}
+
+func isItIt(x int) bool {
+	for i := 1; i < 20; i++ {
+		if x%i  != 0 {
+			return false
+		}
+	}
+	return true
+
+}
---
+++ prjEuler/prob6.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+Sum square difference
+Problem 6
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.1``1`
+ */
+
+
+func main() {
+	x := 0
+	y := 0
+	for i := 1; i < 101; i++ {
+		x += i*i
+	}
+
+	for i := 1; i < 101; i++ {
+		y = y + i
+
+	}
+
+	fmt.Println(y)
+	y = y * y
+	fmt.Println(x)
+	fmt.Println(y)
+	z := y - x
+	fmt.Println(z)
+
+
+
+}
